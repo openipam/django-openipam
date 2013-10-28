a7a670ca9abf64e5209f13a31ab3004202c6d5cf
Jay McEntire <jaym@mail.usu.edu>
2013-10-28 11:16:46 -0600

    fixes for wsgi

--- openipam/wsgi.py
+++ openipam/wsgi.py
@@ -15,7 +15,9 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openipam.conf.settings")
+# This doesn't work on mod_wsgi
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openipam.conf.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = "openipam.conf.settings"
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
