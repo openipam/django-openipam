675419648ee1bfaf1f181e8ee1e4ad017101bae0
Jay McEntire <jaym@royale.ser.usu.edu>
2013-11-21 16:02:48 -0700

    bug fixes

--- openipam/conf/local_settings_example.py
+++ openipam/conf/local_settings_example.py
@@ -19,6 +19,8 @@ DATABASES = {
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ATOMIC_REQUESTS': True,
+        'CONN_MAX_AGE': 600
     }
 }
 
--- openipam/hosts/managers.py
+++ openipam/hosts/managers.py
@@ -1,10 +1,4 @@
-from django.contrib.auth.models import User, Group, Permission
-from django.contrib.contenttypes.models import ContentType
 from django.db.models import Model, Manager, Q
-from django.db import transaction
-from django.core.exceptions import ValidationError
-from django.utils.timezone import utc, now
-from django.conf import settings
 
 from guardian.managers import UserObjectPermissionManager
 from guardian.models import UserObjectPermission
@@ -12,14 +6,8 @@ from guardian.shortcuts import get_objects_for_user, get_perms, get_users_with_p
 
 from netfields import NetManager
 
-from openipam.dns.models import Domain
-from openipam.network.models import Lease, Pool, Network, Address, DhcpGroup
-
-from datetime import timedelta, datetime
-
 
 class HostManager(NetManager):
-
 
     def find_owners_of_host(self, mac):
         host = self.get(mac=mac)
