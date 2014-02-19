8d0459d5e8b27ad93e8bb53882be3c901deb73b4
Jay McEntire <jaym@royale.ser.usu.edu>
2014-02-18 17:12:29 -0700

    middleware doesnt need this

--- openipam/middleware.py
+++ openipam/middleware.py
@@ -26,8 +26,8 @@ class LoginRequiredMiddleware:
  'django.contrib.auth.middlware.AuthenticationMiddleware'. If that doesn't\
  work, ensure your TEMPLATE_CONTEXT_PROCESSORS setting includes\
  'django.core.context_processors.auth'."
-
-        if not request.user.is_authenticated():
-            path = request.path_info.lstrip('/')
-            if not any(m.match(path) for m in EXEMPT_URLS):
-                return HttpResponseRedirect(settings.LOGIN_URL)
+        if not request.path.startswith('/api'):
+            if not request.user.is_authenticated():
+                path = request.path_info.lstrip('/')
+                if not any(m.match(path) for m in EXEMPT_URLS):
+                    return HttpResponseRedirect(settings.LOGIN_URL)
