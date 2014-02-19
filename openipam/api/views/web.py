99f6101c21e4bbdb383d6375d70cf2c954834cc8
Jay McEntire <jaym@royale.ser.usu.edu>
2014-02-19 09:53:17 -0700

    fix for ajax

--- openipam/api/views/web.py
+++ openipam/api/views/web.py
@@ -10,12 +10,11 @@ from openipam.network.models import Network, AddressType, NetworkRange
 @api_view(('GET',))
 @renderer_classes((TemplateHTMLRenderer,))
 def network_selects(request, address_type_id):
+    data = {}
 
-    address_type = AddressType.objects.get(id=address_type_id)
-    networks = Network.objects.get_networks_from_address_type(address_type)
-
-    data = {
-        'networks': networks
-    }
+    address_type = AddressType.objects.filter(id=address_type_id)
+    if address_type:
+        networks = Network.objects.get_networks_from_address_type(address_type[0])
+        data['networks'] = networks
 
     return Response(data, template_name='api/web/network_selects.html')
