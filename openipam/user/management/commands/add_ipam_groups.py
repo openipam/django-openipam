fa029e09d941e18c7752f03b3f5bf0a0887f42b2
Jay McEntire <jaym@royale.ser.usu.edu>
2014-03-13 14:01:27 -0600

    updated for performance

--- openipam/user/management/commands/add_ipam_groups.py
+++ openipam/user/management/commands/add_ipam_groups.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
+UserGroup = User.groups.through
 
 class Command(BaseCommand):
     args = ''
@@ -11,17 +13,20 @@ class Command(BaseCommand):
 
         self.stdout.write('Adding ipam groups to users...')
 
-        User = get_user_model()
-
         ipam_user_group, u_created = Group.objects.get_or_create(name='ipam-users')
         ipam_admin_group, a_created = Group.objects.get_or_create(name='ipam-admins')
 
         # Add Ipam Users
-        for user in User.objects.all():
-            user.groups.add(ipam_user_group)
-            if user.is_superuser:
-                user.groups.add(ipam_admin_group)
+        all_users = User.objects.exclude(groups=ipam_user_group).exclude(pk=-1).distinct()
+        super_users = User.objects.exclude(groups=ipam_admin_group).exclude(pk=-1).filter(is_superuser=True).distinct()
+        add_user_list = []
 
-            user.save()
+        for user in all_users:
+            add_user_list.append(UserGroup(user=user, group=ipam_user_group))
 
-            self.stdout.write('User groups for %s updated' % user.username)
+        for user in super_users:
+            add_user_list.append(UserGroup(user=user, group=ipam_admin_group))
+
+        UserGroup.objects.bulk_create(add_user_list)
+
+        self.stdout.write('IPAM Groups added.')
