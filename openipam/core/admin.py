cf08beda304dd17123cc1ac3200ea92a3603d0c2
Jay McEntire <jaym@royale.ser.usu.edu>
2014-04-14 12:15:15 -0600

    updates

--- openipam/core/admin.py
+++ openipam/core/admin.py
@@ -4,9 +4,10 @@ from openipam.core.models import FeatureRequest
 
 class FeatureRequestAdmin(admin.ModelAdmin):
     list_display = ('submitted', 'comment', 'full_user', 'type', 'is_complete',)
-    list_filter = ('type',)
+    list_filter = ('type', 'is_complete')
     search_fields = ('comment', 'user__username')
     readonly_fields = ('user',)
+    actions = ['mark_complete']
 
     def save_model(self, request, obj, form, change):
         obj.user = request.user
@@ -16,5 +17,8 @@ class FeatureRequestAdmin(admin.ModelAdmin):
         return '%s (%s)' % (obj.user.get_full_name(), obj.user.username)
     full_user.short_description = 'user'
 
+    def mark_complete(self, request, queryset):
+        queryset.update(is_complete=True)
+    mark_complete.short_description = "Mark select as complete"
 
 admin.site.register(FeatureRequest, FeatureRequestAdmin)
