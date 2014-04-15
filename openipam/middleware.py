246a786a20a3bcbe8abef2c4417c1a54f09efd5b
Jay McEntire <jaym@royale.ser.usu.edu>
2014-04-15 10:40:01 -0600

    middleware update

--- openipam/middleware.py
+++ openipam/middleware.py
@@ -30,11 +30,10 @@ class LoginRequiredMiddleware(object):
  'django.contrib.auth.middlware.AuthenticationMiddleware'. If that doesn't\
  work, ensure your TEMPLATE_CONTEXT_PROCESSORS setting includes\
  'django.core.context_processors.auth'."
-        if not request.path.startswith('/api') and not request.path == '/password/forgot/':
-            if not request.user.is_authenticated():
-                path = request.path_info.lstrip('/')
-                if not any(m.match(path) for m in EXEMPT_URLS):
-                    return HttpResponseRedirect(settings.LOGIN_URL)
+        if not request.user.is_authenticated():
+            path = request.path_info.lstrip('/')
+            if not any(m.match(path) for m in EXEMPT_URLS):
+                return HttpResponseRedirect(settings.LOGIN_URL)
 
 
 class MimicUserMiddleware(object):
