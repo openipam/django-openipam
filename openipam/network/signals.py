2203c4a61f260ff1ebe34c05f9c27850f8bd4611
Jay McEntire <jaym@royale.ser.usu.edu>
2014-05-15 16:48:44 -0600

    moving signal to address model

--- openipam/network/signals.py
+++ openipam/network/signals.py
@@ -12,3 +12,11 @@ def validate_address_type(sender, instance, action, **kwargs):
     if action == 'pre_add':
         if instance.pool:
             raise ValidationError('Address Types cannot have both a pool and a range.')
+
+
+def delete_dns_record_for_static_host(sender, instance, *args, **kwargs):
+    """
+        Deletes A or AAAA and PTR records from address where hosts are removed.
+    """
+    if not instance.host:
+        instance.release()
