b4f10f33835aa9dcf7d2fb7a3ff80b660641bcc6
Jay McEntire <jay.mcentire@usu.edu>
2014-05-28 09:42:46 -0600

    change pushed because of manager changes

--- openipam/hosts/decorators.py
+++ openipam/hosts/decorators.py
@@ -5,6 +5,24 @@ from openipam.hosts.models import Host
 #require_http_methods
 
 
+def permission_owner_required(view_func):
+    """
+    Decorator for views that checks that the User has owner permission
+    on a host.
+    """
+    def _wrapped_view(request, pk, *args, **kwargs):
+        permited_host = Host.objects.get_host_with_owner_perms(request.user, pk=pk)
+        if permited_host:
+            return view_func(request, *args, **kwargs)
+        else:
+            from openipam.hosts.views import HostDetailView
+
+            host_view = HostDetailView.as_view()
+            response = host_view(request, pk=pk, read_only=True, *args, **kwargs)
+            return response.render()
+    return wraps(view_func)(_wrapped_view)
+
+
 def permission_owner_host(view_func):
     """
     Decorator for views that checks that the User has owner permission
