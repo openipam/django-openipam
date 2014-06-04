5e7c1d62a56a480b733f28373b17cd73de38fb0e
Jay McEntire <jay.mcentire@usu.edu>
2014-06-04 11:37:01 -0600

    merge function for performance

---
+++ openipam/core/utils/merge_values.py
@@ -0,0 +1,23 @@
+import itertools
+
+
+def merge_values(values):
+    grouped_results = itertools.groupby(values, key=lambda value: value['mac'])
+
+    merged_values = []
+    for k, g in grouped_results:
+        groups = list(g)
+        merged_value = {}
+        for group in groups:
+            for key, val in group.iteritems():
+                if not merged_value.get(key):
+                    merged_value[key] = val
+                elif val != merged_value[key]:
+                    if isinstance(merged_value[key], list):
+                        if val not in merged_value[key]:
+                            merged_value[key].append(val)
+                    else:
+                        old_val = merged_value[key]
+                        merged_value[key] = [old_val, val]
+        merged_values.append(merged_value)
+    return merged_values
