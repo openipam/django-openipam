4f38a79721523546bf824d782e020d462b942fb8
Jay McEntire <jay.mcentire@usu.edu>
2014-08-04 15:05:50 -0600

    changed fields on model mixin

---
+++ openipam/core/mixins.py
@@ -0,0 +1,28 @@
+from django.db.models.signals import post_save
+
+
+class DirtyFieldsMixin(object):
+    def __init__(self, *args, **kwargs):
+        super(DirtyFieldsMixin, self).__init__(*args, **kwargs)
+        post_save.connect(reset_state, sender=self.__class__,
+            dispatch_uid='%s-DirtyFieldsMixin-sweeper' % self.__class__.__name__)
+        reset_state(sender=self.__class__, instance=self)
+
+    def _as_dict(self):
+        return dict([(f, getattr(self, f)) for f in self.__dict__.keys() if not f.startswith('_')])
+
+    def get_dirty_fields(self):
+        new_state = self._as_dict()
+
+        return dict([(key, value) for key, value in self._original_state.items() if value != new_state[key]])
+
+    def is_dirty(self):
+        # in order to be dirty we need to have been saved at least once, so we
+        # check for a primary key and we need our dirty fields to not be empty
+        if not self.pk:
+            return True
+        return {} != self.get_dirty_fields()
+
+
+def reset_state(sender, instance, **kwargs):
+    instance._original_state = instance._as_dict()
