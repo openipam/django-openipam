c322ec98624ce592cdb66567b2a3d143ad411cc4
Jay McEntire <jay.mcentire@usu.edu>
2014-08-06 15:39:20 -0600

    code updates to ldap backend to delete groups that are changed on user.  speedups.

--- openipam/core/backends.py
+++ openipam/core/backends.py
@@ -5,6 +5,7 @@ from django.core.mail.backends.smtp import EmailBackend
 from django.core.mail.backends.console import EmailBackend as ConsoleEmailBackend
 
 from openipam.log.models import EmailLog
+from openipam.user.models import GroupSource, AuthSource
 
 # Dont require this.
 try:
@@ -54,6 +55,7 @@ class IPAMLDAPBackend(LDAPBackend):
 
 class _IPAMLDAPUser(_LDAPUser):
 
+    # TODO: How can we take users out of LDAP groups?
     def _mirror_groups(self):
         """
         Mirrors the user's LDAP groups in the Django database and updates the
@@ -62,22 +64,46 @@ class _IPAMLDAPUser(_LDAPUser):
         # groups = set([Group.objects.get_or_create(name=group_name)[0] for group_name
         #     in group_names] + [group for group in self._user.groups.all()])
 
-        group_names = self._get_groups().get_group_names()
-        existing_groups = self._user.groups.all()
+        # Get LDAP source
+        source = AuthSource.objects.get(name='LDAP')
+        # Get the LDAP group names from LDAP for user
+        user_ldap_group_names = self._get_groups().get_group_names()
+        # Get existing ldap groups
+        existing_ldap_groups = Group.objects.select_related('source').filter(source__source=source)
+        # Get existing ldap groups names
+        existing_ldap_groups_names = set([group.name for group in existing_ldap_groups])
+        # Diff the groups to add from the user
+        groups_to_add = list(user_ldap_group_names - existing_ldap_groups_names)
 
-        db_groups_names = set([group.name for group in Group.objects.all()])
-        groups_to_add = list(group_names - db_groups_names)
-
-        new_groups_to_assign = Group.objects.none()
+        # Add new LDAP groups
+        new_ldap_user_groups = Group.objects.none()
         if groups_to_add:
+            created_groups = []
             for group in groups_to_add:
-                Group.objects.get_or_create(name=group)
-            #Group.objects.bulk_create([Group(name=group) for group in groups_to_add])
-            new_groups_to_assign = Group.objects.filter(name__in=groups_to_add)
+                group, created = Group.objects.get_or_create(name=group)
+                created_groups.append(group)
+            # Group.objects.bulk_create([Group(name=group) for group in groups_to_add])
+            new_ldap_user_groups = Group.objects.select_related('source').filter(pk__in=[group.pk for group in created_groups])
+        # Make sure all LDAP groups are sources as LDAP
+        ldap_groups = new_ldap_user_groups | existing_ldap_groups
+        for group in ldap_groups:
+            try:
+                assert group.source
+            except:
+                group.save()
+        GroupSource.objects.filter(group__in=ldap_groups).update(source=source)
 
-        existing_groups_to_assign = Group.objects.filter(name__in=group_names)
-        groups = new_groups_to_assign | existing_groups | existing_groups_to_assign
+        # Get existing users LDAP groups
+        existing_user_ldap_groups = self._user.groups.filter(source__source=source)
+        # Get exsting user LDAP groups names
+        existing_user_ldap_groups_names = set([group.name for group in existing_user_ldap_groups])
+        # LDAP groups that need to be removed from a user
+        user_ldap_groups_to_delete = list(user_ldap_group_names - existing_user_ldap_groups_names)
+        # Get user groups with the LDAP groups purged that they are not apart of anymore
+        purged_user_groups = self._user.groups.exclude(name__in=user_ldap_groups_to_delete)
 
+        # Set the Groups to the User
+        groups = purged_user_groups | new_ldap_user_groups
         self._user.groups = groups
 
 
