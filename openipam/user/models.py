6adb9dae674d213f77eb8d17a9727f530ea62281
Jay McEntire <jay.mcentire@usu.edu>
2014-08-19 16:36:22 -0600

    bug fix

--- openipam/user/models.py
+++ openipam/user/models.py
@@ -30,8 +30,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # TODO: Remove later
     min_permissions = models.ForeignKey('Permission', db_column='min_permissions', blank=True, null=True,
-                                        related_name='user_min_permissions', default=None)
-    source = models.ForeignKey('AuthSource', db_column='source', default=None, blank=True, null=True)
+                                        related_name='user_min_permissions')
+    source = models.ForeignKey('AuthSource', db_column='source', blank=True, null=True)
 
     objects = IPAMUserManager()
 
