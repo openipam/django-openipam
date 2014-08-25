0889754d7f63bfb6915f977b357023ea010f3ac6
Jay McEntire <jay.mcentire@usu.edu>
2014-08-25 14:48:14 -0600

    validation fix

--- openipam/api/serializers/hosts.py
+++ openipam/api/serializers/hosts.py
@@ -168,6 +168,9 @@ class HostCreateUpdateSerializer(serializers.Serializer):
         mac = attrs.get(source)
         mac = ''.join([c for c in mac if c.isdigit() or c.isalpha()])
 
+        if len(mac) != 12:
+            raise serializers.ValidationError('The mac address entered is not valid: %s.' % mac)
+
         host_exists = Host.objects.filter(mac=mac)
         if self.object:
             host_exists = host_exists.exclude(mac=self.object.pk)
