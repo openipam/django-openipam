97e5be3227dfd9df37f4ea3904b943341b900251
Jay McEntire <jay.mcentire@usu.edu>
2014-09-17 14:11:57 -0600

    usu urls

--- openipam/usu/urls.py
+++ openipam/usu/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url, include
+from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('openipam.usu.views',
 
     url(r'^overview/$', 'overview', name='overview'),
-    url(r'^leases/$', 'available_leases', name='available_leases'),
+    url(r'^leases/usage/$', 'lease_usage', name='lease_usage'),
+    url(r'^leases/available/$', 'leases_available', name='leases_available'),
     url(r'^weathermap/$', 'weather_map', name='weather_map'),
     #url(r'^add/$', 'add', name='add_hosts'),
 )
