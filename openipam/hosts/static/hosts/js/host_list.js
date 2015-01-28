0968de29df9aeb19974fce359af84310d9f29ccc
Jay McEntire <jay.mcentire@usu.edu>
2015-01-27 17:37:46 -0700

    updates to caching

--- openipam/hosts/static/hosts/js/host_list.js
+++ openipam/hosts/static/hosts/js/host_list.js
@@ -38,7 +38,7 @@ $(function(){
                 ajax = true;
                 settings.clearCache = false;
             }
-            else if (cacheLower < 0 || requestStart < cacheLower || requestEnd > cacheUpper) {
+            else if (cacheLower < 0 || requestStart < cacheLower || requestEnd > cacheUpper || requestLength > 50) {
                 // outside cached data - need to make a request
                 ajax = true;
             }
