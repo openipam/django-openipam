ec55fa33e53835acb89c35d9e95c8c45cbd32af5
Jay McEntire <jay.mcentire@usu.edu>
2015-02-25 14:54:51 -0700

    text searching on hosts

---
+++ openipam/hosts/migrations/0007_host_search_index.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import djorm_pgfulltext.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hosts', '0006_auto_20150218_1326'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='host',
+            name='search_index',
+            field=djorm_pgfulltext.fields.VectorField(default=b'', serialize=False, null=True, editable=False, db_index=True),
+            preserve_default=True,
+        ),
+    ]
