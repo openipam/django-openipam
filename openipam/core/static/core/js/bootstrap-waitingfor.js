343782d61d90956ca8285525e57aa980ca146423
Jay McEntire <jay.mcentire@usu.edu>
2015-03-26 11:09:51 -0600

    processing dialog

---
+++ openipam/core/static/core/js/bootstrap-waitingfor.js
@@ -0,0 +1,23 @@
+var ProcessingDialog;
+ProcessingDialog = ProcessingDialog || (function () {
+    var pleaseWaitDiv = $(
+		'<div class="modal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
+		'<div class="modal-dialog modal-m">' +
+		'<div class="modal-content">' +
+			'<div class="modal-header"><h3 style="margin:0;">Processing</h3></div>' +
+			'<div class="modal-body">' +
+				'<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
+			'</div>' +
+		'</div></div></div>'
+	);
+
+    return {
+        showPleaseWait: function() {
+            pleaseWaitDiv.modal();
+        },
+        hidePleaseWait: function () {
+            pleaseWaitDiv.modal('hide');
+        },
+
+    };
+})();
