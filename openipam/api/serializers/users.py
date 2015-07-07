3907e8ba9602bfaec0db718e5f57240f49cf34a5
Jay McEntire <jay.mcentire@usu.edu>
2015-07-07 16:52:50 -0600

    group api foo

--- openipam/api/serializers/users.py
+++ openipam/api/serializers/users.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 from rest_framework import serializers
 
@@ -8,3 +9,8 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
