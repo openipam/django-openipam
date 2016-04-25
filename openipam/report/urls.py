8498e044993d67a9163fe28680e92ca5fa071dc9
Jay McEntire <jaym@royale.ser.usu.edu>
2016-04-25 16:48:50 -0600

    code cleanup, class based views

--- openipam/report/urls.py
+++ openipam/report/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 
+from openipam.report.views import DashboardView, LeaseUsageView, WeatherMapView, DisabledHostsView, ServerHostsView
+
 
 urlpatterns = patterns('openipam.report.views',
-
-    url(r'^leases/usage/$', 'lease_usage', name='reports_lease_usage'),
-    url(r'^leases/available/$', 'leases_available', name='reports_leases_available'),
-    url(r'^weathermap/$', 'weather_map', name='reports_weather_map'),
-    url(r'^disabled/$', 'disabled_hosts', name='reports_disabled'),
-    url(r'^server_hosts/$', 'server_hosts', name='reports_server_hosts'),
-    url(r'^$', 'overview', name='reports_dashboard'),
+    url(r'^leases/usage/$', LeaseUsageView.as_view(), name='reports_lease_usage'),
+    url(r'^weathermap/$', WeatherMapView.as_view(), name='reports_weather_map'),
+    url(r'^disabled/$', DisabledHostsView.as_view(), name='reports_disabled'),
+    url(r'^server_hosts/$', ServerHostsView.as_view(), name='reports_server_hosts'),
+    url(r'^$', DashboardView.as_view(), name='reports_dashboard'),
 )
--- openipam/report/views.py
+++ openipam/report/views.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 from django.db.models import Q
 from django.core.urlresolvers import reverse
 from django.contrib.contenttypes.models import ContentType
+from django.views.generic import TemplateView
 
 from datetime import timedelta
 
@@ -15,80 +16,55 @@ from openipam.network.models import Network, NetworkRange, AddressType
 
 from guardian.models import UserObjectPermission, GroupObjectPermission
 
+from braces.views import GroupRequiredMixin
+
 import qsstats
 
 import requests
 import operator
 
 
-def overview(request):
-
-    # hosts = Host.objects.all()
-    # hosts_stats = qsstats.QuerySetStats(hosts, 'changed', aggregate=Count('mac'))
-    # #users = User.objects.all()
-    # #users_stats = qsstats.QuerySetStats(users, 'date_joined')
-
-    # xdata = ['Today', 'This Week', 'This Month']
-    # ydata = [hosts_stats.this_day(), hosts_stats.this_week(), hosts_stats.this_month()]
-
-    # extra_serie1 = {"tooltip": {"y_start": "", "y_end": " hosts"}}
-    # chartdata = {
-    #     'x': xdata, 'name1': 'Hosts', 'y1': ydata, 'extra1': extra_serie1,
-    # }
-    # charttype = "discreteBarChart"
-    # chartcontainer = 'bar_container'
-
-    # hostname = request.META.get('HTTP_HOST')
-    # top_lease_usage = requests.get(url='http://%s%s?format=json' % (hostname, reverse('api_reports_subnet_data'))).json()[:5]
-
-    # context = {
-    #     'charttype': charttype,
-    #     'chartdata': chartdata,
-    #     'chartcontainer': chartcontainer,
-    #     'extra': {
-    #         'x_is_date': False,
-    #         'x_axis_format': '',
-    #         'tag_script_js': True,
-    #         'jquery_on_ready': False,
-    #     }
-    # }
-    context = {}
-    return render(request, 'report/dashboard.html', context)
+class DashboardView(GroupRequiredMixin, TemplateView):
+    group_required = 'ipam_admins'
+    template_name = 'report/dashboard.html'
 
 
-def lease_usage(request):
-    return render(request, 'report/lease_usage.html')
+class LeaseUsageView(GroupRequiredMixin, TemplateView):
+    group_required = 'ipam_admins'
+    template_name = 'report/lease_usage.html'
 
 
-def leases_available(request):
-    return render(request, 'report/leases_available.html', {'is_popup': True})
+class WeatherMapView(GroupRequiredMixin, TemplateView):
+    group_required = 'ipam_admins'
+    template_name = 'report/weather_map.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(WeatherMapView, self).get_context_data(**kwargs)
+        popup = self.request.GET.get('_popup', None)
+        context['is_popup'] = True if popup else False
+        return context
 
 
-def weather_map(request):
-    popup = request.GET.get('_popup', None)
-    is_popup = True if popup else False
-    return render(request, 'report/weather_map.html', {'is_popup': is_popup})
+class DisabledHostsView(GroupRequiredMixin, TemplateView):
+    group_required = 'ipam_admins'
+    template_name = 'report/disabled.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DisabledHostsView, self).get_context_data(**kwargs)
+        hardcoded = (
+            GulRecentArpBymac.objects
+                .select_related('host')
+                .filter(
+                    host__disabled_host__isnull=False,
+                    stopstamp__gt=timezone.now() - timedelta(minutes=10),
+                )
+                .exclude(host__leases__ends__lt=timezone.now())
+                .extra(where=["NOT (gul_recent_arp_bymac.address <<= '172.16.0.0/16' OR gul_recent_arp_bymac.address <<= '172.18.0.0/16')"])
+        )
+        context['hardcoded'] = hardcoded
+        return context
 
 
-def disabled_hosts(request):
-    hardcoded = (
-        GulRecentArpBymac.objects
-            .select_related('host')
-            .filter(
-                host__disabled_host__isnull=False,
-                stopstamp__gt=timezone.now() - timedelta(minutes=10),
-            )
-            .exclude(host__leases__ends__lt=timezone.now())
-            .extra(where=["NOT (gul_recent_arp_bymac.address <<= '172.16.0.0/16' OR gul_recent_arp_bymac.address <<= '172.18.0.0/16')"])
-    )
-
-    context = {
-        'hardcoded': hardcoded
-    }
-
-    return render(request, 'report/disabled.html', context)
-
-
-def server_hosts(request):
-    context = {}
-    return render(request, 'report/server_hosts.html', context)
+class ServerHostsView(GroupRequiredMixin, TemplateView):
+    group_required = 'ipam_admins'
+    template_name = 'report/server_hosts.html'
