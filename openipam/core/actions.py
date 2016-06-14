ee359a5159e992b5f75f3220d3e4e32507d99410
Jay McEntire <jaym@royale.ser.usu.edu>
2016-06-14 10:53:28 -0600

    fixed delete action from django upgrade

--- openipam/core/actions.py
+++ openipam/core/actions.py
@@ -1,20 +1,19 @@
-from django.core.exceptions import PermissionDenied
 from django.contrib import messages
 from django.contrib.admin import helpers
 from django.contrib.admin.utils import get_deleted_objects, model_ngettext
+from django.core.exceptions import PermissionDenied
 from django.db import router
 from django.template.response import TemplateResponse
 from django.utils.encoding import force_text
 from django.utils.translation import ugettext_lazy, ugettext as _
 
+
 def changed_delete_selected(modeladmin, request, queryset):
     """
     Default action which deletes the selected objects.
-
-    This action first displays a confirmation page whichs shows all the
+    This action first displays a confirmation page which shows all the
     deleteable objects, or, if the user has no permission one of the related
     childs (foreignkeys), a "permission denied" message.
-
     Next, it deletes all selected objects and redirects back to the change list.
     """
     opts = modeladmin.model._meta
@@ -28,12 +27,12 @@ def changed_delete_selected(modeladmin, request, queryset):
 
     # Populate deletable_objects, a data structure of all related objects that
     # will also be deleted.
-    deletable_objects, perms_needed, protected = get_deleted_objects(
+    deletable_objects, model_count, perms_needed, protected = get_deleted_objects(
         queryset, opts, request.user, modeladmin.admin_site, using)
 
     # The user has already confirmed the deletion.
     # Do the deletion and return a None to display the change list view again.
-    if request.POST.get('post'):
+    if request.POST.get('post') and not protected:
         if perms_needed:
             raise PermissionDenied
         n = queryset.count()
@@ -41,7 +40,6 @@ def changed_delete_selected(modeladmin, request, queryset):
             for obj in queryset:
                 obj_display = force_text(obj)
                 obj.changed_by = request.user
-                obj.save()
                 modeladmin.log_deletion(request, obj, obj_display)
             queryset.delete()
             modeladmin.message_user(request, _("Successfully deleted %(count)d %(items)s.") % {
@@ -60,23 +58,27 @@ def changed_delete_selected(modeladmin, request, queryset):
     else:
         title = _("Are you sure?")
 
-    context = {
-        "title": title,
-        "objects_name": objects_name,
-        "deletable_objects": [deletable_objects],
-        'queryset': queryset,
-        "perms_lacking": perms_needed,
-        "protected": protected,
-        "opts": opts,
-        "app_label": app_label,
-        'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
-    }
+    context = dict(
+        modeladmin.admin_site.each_context(request),
+        title=title,
+        objects_name=objects_name,
+        deletable_objects=[deletable_objects],
+        model_count=dict(model_count).items(),
+        queryset=queryset,
+        perms_lacking=perms_needed,
+        protected=protected,
+        opts=opts,
+        action_checkbox_name=helpers.ACTION_CHECKBOX_NAME,
+        media=modeladmin.media,
+    )
+
+    request.current_app = modeladmin.admin_site.name
 
     # Display the confirmation page
     return TemplateResponse(request, modeladmin.delete_selected_confirmation_template or [
         "admin/%s/%s/delete_selected_confirmation.html" % (app_label, opts.model_name),
         "admin/%s/delete_selected_confirmation.html" % app_label,
         "admin/delete_selected_confirmation.html"
-    ], context, current_app=modeladmin.admin_site.name)
+    ], context)
 
 changed_delete_selected.short_description = ugettext_lazy("Delete selected %(verbose_name_plural)s")
