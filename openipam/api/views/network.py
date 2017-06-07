cbe5761405ffe5be267421831ab5eabffefb1b11
Ryan Medlyn <rmedlyn@users.noreply.github.com>
2017-06-07 16:12:37 -0600

    Removes unused imports

--- openipam/api/views/network.py
+++ openipam/api/views/network.py
@@ -8,8 +8,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from rest_framework import mixins
-from rest_framework.decorators import permission_classes
 
 from openipam.network.models import Network, Address, DhcpGroup
 from openipam.api.views.base import APIPagination
