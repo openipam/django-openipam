ace0648f91336afa114264a8943b378f85667475
Jay McEntire <jay.mcentire@usu.edu>
2019-07-22 09:35:07 -0600

    flake8 fixes

--- openipam/api/views/network.py
+++ openipam/api/views/network.py
@@ -1,7 +1,6 @@
 from django.core.exceptions import ValidationError
 
 from rest_framework import generics
-from rest_framework import filters
 from rest_framework import permissions
 from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
