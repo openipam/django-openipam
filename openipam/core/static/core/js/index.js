d6c5c40463fe6c6fd8acbeeca7a55e34abd33d1f
Stuart Hopkins <stuart.hopkins@aggiemail.usu.edu>
2020-04-27 11:07:13 -0600

    Quickview renews records

--- openipam/core/static/core/js/index.js
+++ openipam/core/static/core/js/index.js
@@ -5,6 +5,7 @@ $(function () {
     //         $(this).hide();
     //     });
 
+
 });
 
 function deleteHost(delId, mac) {
@@ -28,6 +29,31 @@ function deleteHost(delId, mac) {
         },
         success: res => {
             console.log(`${id} was deleted`);
+        }
+    });
+}
+
+function renewHost(renewId, mac) {
+    // Strip the id to get parent id
+    let baseId = renewId.substring(6);
+    // let element = document.getElementById(id);
+
+
+    // Send an AJAX request to renew the record
+    $.ajax({
+        url: `/api/hosts/${mac}/renew/`, 
+        type: 'POST',
+        headers: {
+            'X-CSRFToken': $.cookie('csrftoken'),
+            'sessionid': $.cookie('sessionid'),
+        },
+        data: {
+            expire_days: 365,
+        },
+        success: res => {
+            // Update displayed expiration
+            document.getElementById(`expire-${baseId}`).innerText = `${res.expire_days} days`;
+            console.log(`${baseId} was renewed`);
         }
     });
 }
