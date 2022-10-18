086984bc7ea4e31e9d6e36354cd51ba4bea66bc5
Parker Freestone <parkerfreestone@gmail.com>
2022-10-18 10:55:13 -0600

    Added redirect, and titles to template pages

--- openipam/report/urls.py
+++ openipam/report/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.base import RedirectView
 
 from openipam.report import views
 
@@ -12,5 +14,5 @@ urlpatterns = [
     url(r"^ptr_dns/$", views.PTRDNSView.as_view(), name="reports_ptr_dns"),
     url(r"^ipam_stats$", views.IpamStatsView.as_view(), name="reports_ipam_stats"),
     url(r"^expired_hosts", views.ExpiredHostsView.as_view(), name="expired_hosts"),
-    url(r"^$", views.DashboardView.as_view(), name="reports_dashboard"),
+    url(r"^$", RedirectView.as_view(url=reverse_lazy("reports_ipam_stats"))),
 ]
