2e1c35fdc79afc20365faefa7bba2414b657f924
Jay McEntire <jay.mcentire@gmail.com>
2022-10-18 10:36:22 -0600

    conflict fix (#175)
    * csv export fixes
    
    * csv export fixes
    
    * adding bulk add perms management command
    
    * conflict fix

--- openipam/report/urls.py
+++ openipam/report/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     url(r"^ptr_dns/$", views.PTRDNSView.as_view(), name="reports_ptr_dns"),
     url(r"^ipam_stats$", views.IpamStatsView.as_view(), name="reports_ipam_stats"),
     url(r"^expired_hosts", views.ExpiredHostsView.as_view(), name="expired_hosts"),
-    url(r"^$", views.DashboardView.as_view(), name="reports_dashboard"),
 ]
