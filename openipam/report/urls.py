abe9ded14fb5379eb796c25c6eb989c382ea4a92
Parker Freestone <99781397+parkerfreestone@users.noreply.github.com>
2022-10-24 10:43:39 -0600

    Updated template to be responsive; Added title, and URL to match report. (#179)

--- openipam/report/urls.py
+++ openipam/report/urls.py
@@ -6,7 +6,9 @@ from openipam.report import views
 urlpatterns = [
     url(r"^disabled/$", views.DisabledHostsView.as_view(), name="reports_disabled"),
     url(
-        r"^server_hosts/$", views.ServerHostsView.as_view(), name="reports_server_hosts"
+        r"^exposed_hosts/$",
+        views.ExposedHostsView.as_view(),
+        name="reports_exposed_hosts",
     ),
     url(r"^host_dns/$", views.HostDNSView.as_view(), name="reports_host_dns"),
     url(r"^ptr_dns/$", views.PTRDNSView.as_view(), name="reports_ptr_dns"),
