9f2bb7a7787ad5098913c891f22b86d962f2e197
Parker Freestone <parkerfreestone@gmail.com>
2022-10-24 14:12:30 -0600

    Changed all urls to match django2 pattern

--- openipam/urls.py
+++ openipam/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import path, re_path
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
@@ -15,28 +16,30 @@ admin.autodiscover()
 
 urlpatterns = [
     # Testing 404 and 500 pages
-    url(r"^500/$", server_error),
-    url(r"^404/$", page_not_found),
+    path("500/", server_error),
+    path("404/", page_not_found),
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
     # openIPAM urls
-    url(r"^", include("openipam.core.urls")),
+    path("", include("openipam.core.urls")),
     # Admin Frontend
-    url(r"^", include(admin.site.urls)),
+    path("", include(admin.site.urls)),
     # Utitity routes to serve admin
-    url(r"^admin_tools/", include("admin_tools.urls")),
+    path("admin_tools/", include("admin_tools.urls")),
 ]
 
 # Serve Static and Media on development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG is False:  # if DEBUG is True it will be served automatically
     urlpatterns += [
-        url(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT})
+        re_path(
+            r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}
+        )
     ]
 else:
     import debug_toolbar
 
-    urlpatterns += [url(r"^__debug__/", include(debug_toolbar.urls))]
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
     urlpatterns += staticfiles_urlpatterns()
 
 handler403 = "openipam.core.views.page_denied"
