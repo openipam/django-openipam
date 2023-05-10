4fb9fe866c92ec57143b44a7cde9607ee27e9c9e
Jay McEntire <jay.mcentire@gmail.com>
2023-05-10 14:29:50 -0600

    adding modified autheticator for JWT so new users can be synced

--- openipam/conf/settings.py
+++ openipam/conf/settings.py
@@ -262,7 +262,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+        "openipam.api.authentication.IPAMJSONWebTokenAuthentication",
+        # "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
