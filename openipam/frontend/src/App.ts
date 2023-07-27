0ba288f30ca9a5db67d918aeafdfdd25abde2ab9
Ezri Brimhall <ezri.brimhall@usu.edu>
2023-07-27 15:08:32 -0600

    Fixed minor compile error

--- openipam/frontend/src/App.ts
+++ openipam/frontend/src/App.ts
@@ -2,5 +2,10 @@ import Main from "./Main";
 import { createRoot } from "react-dom/client";
 
 const appDiv = document.getElementById("app");
+if (appDiv === null) {
+  throw new Error(
+    "Could not find element with id 'app', is this the right template?"
+  );
+}
 const root = createRoot(appDiv);
 root.render(Main());
