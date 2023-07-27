1a403bf95a1bdb3106c58ea6d0b5b5add74a89b8
Ezri Brimhall <ezri.brimhall@usu.edu>
2023-07-27 13:24:13 -0600

    v2 hosts API works

--- openipam/api_v2/views/base.py
+++ openipam/api_v2/views/base.py
@@ -5,12 +5,16 @@ from rest_framework.viewsets import ModelViewSet
 
 
 # Using page number pagination for v2 API, rather than limit/offset pagination.
+# We're using it for the following reasons:
+# 1. It closely matches how the data will be displayed in the UI
+# 2. It's easier to use in the UI, since we can just pass the page number
+# 3. Matching how a database query might be made doesn't make sense for an API
 class APIPagination(PageNumberPagination):
     """Pagination for API endpoints."""
 
-    page_size = 25
+    page_size = 10  # Current default page size
     page_size_query_param = "page_size"
-    max_page_size = 1000
+    max_page_size = 100  # even 25 takes over a second to load
 
 
 class APIModelViewSet(ModelViewSet):
