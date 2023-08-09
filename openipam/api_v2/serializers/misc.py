36abbd3565a57b9863f5be78cf7d0147f3e731ea
Ezri Brimhall <ezri.brimhall@usu.edu>
2023-08-09 15:23:46 -0600

    Improved attribute choices listing

--- openipam/api_v2/serializers/misc.py
+++ openipam/api_v2/serializers/misc.py
@@ -2,14 +2,32 @@ from rest_framework import serializers as lib_serializers
 from openipam.hosts.models import StructuredAttributeValue, Attribute
 
 
+class StructuredAttributeValueSerializer(lib_serializers.ModelSerializer):
+    changed_by = lib_serializers.ReadOnlyField(source="changed_by.username")
+
+    # This serializer should return None if the related attribute is not structured
+    def to_representation(self, instance):
+        if instance.attribute.structured:
+            return super().to_representation(instance)
+        else:
+            return None
+
+    class Meta:
+        model = StructuredAttributeValue
+        fields = "__all__"
+
+
 class AttributeSerializer(lib_serializers.ModelSerializer):
     changed_by = lib_serializers.ReadOnlyField(source="changed_by.username")
     choices = lib_serializers.SerializerMethodField()
+    # Uncomment this if the categories I added are ever merged
+    # category = lib_serializers.ReadOnlyField(source="category.name")
 
     def get_choices(self, obj):
-        if not obj.structured:
+        if obj.structured:
+            return StructuredAttributeValueSerializer(obj.choices.all(), many=True).data
+        else:
             return None
-        return obj.choices.values_list("value", flat=True)
 
     class Meta:
         model = Attribute
--- openipam/api_v2/views/misc.py
+++ openipam/api_v2/views/misc.py
@@ -3,13 +3,21 @@
 from rest_framework import viewsets as lib_viewsets
 from .base import APIPagination
 from ..serializers.misc import AttributeSerializer
-from openipam.hosts.models import Attribute
+from openipam.hosts.models import Attribute, StructuredAttributeValue
+from django.db.models import Prefetch
 
 
 class AttributeViewSet(lib_viewsets.ReadOnlyModelViewSet):
     """API endpoint that allows attributes to be viewed or edited."""
 
     queryset = (
-        Attribute.objects.select_related("changed_by").prefetch_related("choices").all()
+        Attribute.objects.select_related("changed_by")
+        .prefetch_related(
+            Prefetch(
+                "choices",
+                queryset=StructuredAttributeValue.objects.select_related("changed_by"),
+            )
+        )
+        .all()
     )
     serializer_class = AttributeSerializer
