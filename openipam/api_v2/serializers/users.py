d8a3f3738c6adf8f38c4bfeca519d982d6c7d1e5
Ezri Brimhall <ezri.brimhall@usu.edu>
2023-08-17 14:14:40 -0600

    Added full viewset for users

--- openipam/api_v2/serializers/users.py
+++ openipam/api_v2/serializers/users.py
@@ -1,5 +1,6 @@
 from openipam.user.models import User
 from rest_framework import serializers
+from django.contrib.auth.models import Group
 
 
 class UserNestedSerializer(serializers.ModelSerializer):
@@ -17,7 +18,27 @@ class UserNestedSerializer(serializers.ModelSerializer):
         )
 
 
+class GroupField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.name
+
+    def to_internal_value(self, data):
+        return data
+
+
+class RestrictedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "first_name", "last_name", "email")
+        read_only_fields = (
+            "id",
+            "username",
+        )
+
+
 class UserSerializer(serializers.ModelSerializer):
+    groups = GroupField(many=True, read_only=False, queryset=Group.objects.all())
+
     class Meta:
         model = User
         fields = (
@@ -28,13 +49,15 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
             "is_staff",
             "is_ipamadmin",
+            "is_superuser",
+            "is_active",
+            "last_login",
+            "date_joined",
+            "groups",
         )
         read_only_fields = (
             "id",
             "username",
-            "first_name",
-            "last_name",
-            "email",
-            "is_staff",
-            "is_ipamadmin",
+            "last_login",
+            "date_joined",
         )
