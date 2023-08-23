514b60946a65919792df6d08114ef6159448a1d8
Ezri Brimhall <ezri.brimhall@usu.edu>
2023-08-23 11:26:23 -0600

    Added endpoints for manipulating user group membership

--- openipam/api_v2/views/users.py
+++ openipam/api_v2/views/users.py
@@ -10,6 +10,7 @@ from ..serializers.users import RestrictedUserSerializer, UserSerializer
 from rest_framework import permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from django.contrib.auth.models import Group
 
 # Get the user model from Django
 from django.contrib.auth import get_user_model
@@ -48,18 +49,22 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         """Use a restricted serializer for non-admin users."""
-        if self.request.user.is_superuser or self.action == "me":
+        if self.request.user.is_ipamadmin or self.action == "me":
             return self.serializer_class
         return RestrictedUserSerializer
 
     def get_queryset(self):
         """Only allow admins to list all users."""
-        if not self.request.user.is_superuser and self.action == "list":
+        if not self.request.user.is_ipamadmin and self.action == "list":
             return self.queryset.filter(pk=self.request.user.pk)
         return self.queryset
 
     def create(self, request):
         """Create is not allowed, users are created via LDAP or shell."""
+        return Response(status=405)
+
+    def destroy(self, request, *args, **kwargs):
+        """Destroy is not allowed, users are created via LDAP or shell."""
         return Response(status=405)
 
     def retrieve(self, request, username__iexact: str = None):
@@ -81,3 +86,54 @@ class UserViewSet(viewsets.ModelViewSet):
         """Return the current user."""
         serializer = self.get_serializer(self.request.user)
         return Response(serializer.data)
+
+    @action(
+        detail=False,
+        methods=["get"],
+        url_path=r"me/groups",
+        url_name="my-groups",
+    )
+    def my_groups(self, request):
+        """Return the current user."""
+        user = self.request.user
+        return Response(user.groups.values_list("name", flat=True))
+
+    @action(
+        detail=True,
+        methods=["get"],
+        url_path=r"groups",
+        url_name="groups",
+        permission_classes=[permissions.IsAdminUser],
+    )
+    def groups(self, request, username__iexact: str = None):
+        """Return the current user."""
+        user = self.get_object()
+        return Response(user.groups.values_list("name", flat=True))
+
+    @groups.mapping.post
+    def join_groups(self, request, username__iexact: str = None):
+        """Add the given user to the given groups."""
+        user = self.get_object()
+        groups = request.data.get("groups", [])
+        for group in groups:
+            try:
+                Group.objects.get(name=group).user_set.add(user)
+            except Group.DoesNotExist:
+                return Response(
+                    status=404, data={"detail": f"Group {group} not found."}
+                )
+        return Response(user.groups.values_list("name", flat=True), status=201)
+
+    @groups.mapping.delete
+    def leave_groups(self, request, username__iexact: str = None):
+        """Remove the user from the given groups."""
+        user = self.get_object()
+        groups = request.data.get("groups", [])
+        for group in groups:
+            try:
+                Group.objects.get(name=group).user_set.remove(user)
+            except Group.DoesNotExist:
+                return Response(
+                    status=404, data={"detail": f"Group {group} not found."}
+                )
+        return Response(user.groups.values_list("name", flat=True))
