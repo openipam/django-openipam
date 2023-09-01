7323dc22abb8147783469a27549b7e2ec56287fd
Ezri Brimhall <ezri.brimhall@usu.edu>
2023-09-01 14:47:02 -0600

    Updated log entry filtering to use a filterset class

--- openipam/api_v2/throttling.py
+++ openipam/api_v2/throttling.py
@@ -13,8 +13,8 @@ class SustainedRateThrottle(UserRateThrottle):
     scope = "sustained"
 
     def allow_request(self, request, view):
-        if request.user.is_ipamadmin:
-            # If the user is an administrator, they may legitimately need to make
+        if request.user.is_staff or request.user.is_superuser:
+            # If the user is staff, they may legitimately need to make
             # more than 1000 requests per day. Allow them to do so.
             return True
         return super().allow_request(request, view)
--- openipam/api_v2/views/admin.py
+++ openipam/api_v2/views/admin.py
@@ -1,39 +1,24 @@
 from rest_framework.generics import ListAPIView
 from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
+
+from ..filters.admin import LogEntryFilterSet
 from ..filters.base import FieldChoiceFilter
 
 from .base import LogsPagination
 from ..serializers.admin import LogEntrySerializer, EmailLogSerializer
 from openipam.log.models import EmailLog
 from ..permissions import APIAdminPermission
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class LogEntryList(ListAPIView):
     permission_classes = [APIAdminPermission]
     queryset = LogEntry.objects.all().order_by("-action_time")
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        type = self.request.query_params.get("type", None)
-        flag = self.request.query_params.get("action_flag", None)
-        user = self.request.query_params.get("user", None)
-        if type:
-            queryset = queryset.filter(content_type__model=type)
-        if flag:
-            if flag == "Addition":
-                flag = ADDITION
-            elif flag == "Change":
-                flag = CHANGE
-            elif flag == "Deletion":
-                flag = DELETION
-            queryset = queryset.filter(action_flag=flag)
-        if user:
-            queryset = queryset.filter(user__username=user)
-        return queryset
-
     serializer_class = LogEntrySerializer
     pagination_class = LogsPagination
-    filter_backends = [FieldChoiceFilter]
+    filter_backends = [FieldChoiceFilter, DjangoFilterBackend]
+    filterset_class = LogEntryFilterSet
     filter_field = "content_type__model"
     filter_query_prefix = "include"
     filter_choices = [
