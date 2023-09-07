6c83a639a17e2e8c1fe0bfd26fd616dabdcb918c
canyonbryson <canyonbryson@gmail.com>
2023-09-06 18:47:58 -0600

    better upload by csv

--- openipam/frontend/src/modules/hosts/addByCsv.tsx
+++ openipam/frontend/src/modules/hosts/addByCsv.tsx
@@ -4,6 +4,57 @@ import { CreateHost, Host } from "../../utils/types";
 import { useAddressTypes } from "../../hooks/queries/useAddressTypes";
 import Papa from "papaparse";
 
+const exampleCSVHeaders = [
+  "mac",
+  "Hostname",
+  "IP address",
+  "network",
+  "expire_days",
+  "address Type",
+  "description",
+  "DHCP_GROUP",
+];
+const exampleCSV: any[] = [
+  {
+    mac: "11:11:11:11:11:11",
+    hostname: "example.bluezone.usu.edu",
+    address_type: "Management",
+    ip_address: "10.21.20.2",
+    description: "test example",
+    expire_days: 365,
+  },
+  {
+    mac: "11:11:11:11:11:11",
+    hostname: "example.bluezone.usu.edu",
+    network: "10.2.0.0/24",
+    address_type: "Other",
+    description: "",
+    expire_days: 365,
+  },
+  {
+    mac: "11:11:11:11:11:11",
+    hostname: "example.bluezone.usu.edu",
+    address_type: "Dynamic",
+    description: "",
+    expire_days: 365,
+  },
+  {
+    mac: "11:11:11:11:11:11",
+    hostname: "example.bluezone.usu.edu",
+    address_type: "Dynamic",
+    description: "",
+    expire_days: 365,
+  },
+  {
+    mac: "11:11:11:11:11:11",
+    hostname: "example.bluezone.usu.edu",
+    address_type: "Dynamic",
+    description: "",
+    expire_days: 365,
+    dhcp_group: "aruba-cluster",
+  },
+];
+
 export const AddByCSVModule = (p: {
   showModule: boolean;
   setShowModule: (show: boolean) => void;
@@ -14,12 +65,12 @@ export const AddByCSVModule = (p: {
   const [hosts, dispatch] = useReducer(hostsReducer, []);
   const [loaded, setLoaded] = useState(false);
   const [data, setData] = useState<any>();
-  useEffect(() => {
-    console.log({ hosts });
-    console.log({ data });
-  }, [hosts, data]);
   const addHosts = async () => {
-    await Promise.all(hosts.map((host) => api.hosts.create(host)));
+    await Promise.all(
+      hosts.map((host) =>
+        api.hosts.create({ ...host, expire_days: host.expire_days ?? 7 })
+      )
+    );
     p.setShowModule(false);
   };
   const handleFileChange = (e: any) => {
@@ -27,7 +78,11 @@ export const AddByCSVModule = (p: {
       complete: function (results: { data: string[][] }) {
         setData(results.data);
         const headers = results.data[0].map((h) => {
-          return h.toLowerCase().replace(" ", "_");
+          const head = h.toLowerCase().replace(" ", "_");
+          if (head.startsWith("expire")) return "expire_days";
+          if (head === "address") return "ip_address";
+          if (head.startsWith("mac")) return "mac";
+          return head;
         });
         console.log({ headers });
         setHeaders(headers);
@@ -83,25 +138,64 @@ export const AddByCSVModule = (p: {
             onInput={handleFileChange}
             className="flex file-input file-input-primary"
           />
+          {!loaded && (
+            <div className="flex flex-col">
+              <h1 className="mt-4 text-lg font-bold">Example CSV layout:</h1>
+              <p className="my-1">
+                Note: header name is not case or underscore sensitive. You may
+                include or exclude as many columns as you want.
+              </p>
+              <p className="my-1">
+                Only mac, hostname, and address type are required. Expire_days
+                will default to 7 if left blank, and user_owner will default to
+                your user if no user or group owner is given.
+              </p>
+              <p>
+                Please double check all records to ensure addresses are valid,
+                and non-dynamic types given either network or ip address, and
+                that it is valid for the given address type.
+              </p>
+            </div>
+          )}
           <table className="table">
             <thead>
-              <tr>{loaded && headers.map((h) => <th key={h}>{h}</th>)}</tr>
+              <tr>
+                {loaded
+                  ? headers.map((h) => <th key={h}>{h}</th>)
+                  : exampleCSVHeaders.map((h) => <th key={h}>{h}</th>)}
+              </tr>
             </thead>
             <tbody>
-              {loaded &&
-                hosts.map(
-                  (host: { [key: string]: string | number }, i: number) => (
-                    <tr key={i}>
-                      {Object.values(host).map(
-                        (value: string | number, idx: number) => (
+              {loaded
+                ? hosts.map(
+                    (host: { [key: string]: string | number }, i: number) => (
+                      <tr key={i}>
+                        {headers.map((value: string | number, idx: number) => (
                           <td key={idx} className="p-1 border border-primary">
-                            <pre>{value}</pre>
+                            <pre>{host[value] ?? ""}</pre>
                           </td>
-                        )
-                      )}
-                    </tr>
+                        ))}
+                      </tr>
+                    )
                   )
-                )}
+                : exampleCSV.map(
+                    (host: { [key: string]: string | number }, i: number) => (
+                      <tr key={i}>
+                        {exampleCSVHeaders
+                          .map((h) => {
+                            const head = h.toLowerCase().replace(" ", "_");
+                            if (head.startsWith("expire")) return "expire_days";
+                            if (head === "address") return "ip_address";
+                            return head;
+                          })
+                          .map((value: string, idx: number) => (
+                            <td key={idx} className="p-1 border border-primary">
+                              <pre>{host[value] ?? ""}</pre>
+                            </td>
+                          ))}
+                      </tr>
+                    )
+                  )}
             </tbody>
           </table>
           {loaded && (
