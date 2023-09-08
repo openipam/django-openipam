93a20fa743bba3ef4868b32d3f5bfe3cffb35d47
canyonbryson <canyonbryson@gmail.com>
2023-09-08 14:08:59 -0600

    quick filters

---
+++ openipam/frontend/src/modules/hosts/addCustomFilterModule.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+export const AddCustomFilterModule = (p: {
+  showModule: boolean;
+  setShowModule: (x: boolean) => void;
+  onSubmit: (x: { name: string }) => void;
+}) => {
+  return (
+    <>
+      <input
+        type="checkbox"
+        hidden
+        checked={p.showModule}
+        onChange={(prev) => !prev}
+        id="add-host-module"
+        className="modal-toggle"
+      />
+      <dialog id="add-host-module" className="modal">
+        <div className="modal-box bg-base-100 border border-neutral-content">
+          <label
+            htmlFor="add-host-module"
+            onClick={() => p.setShowModule(false)}
+            className="absolute top-0 right-0 p-4 cursor-pointer"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+          </label>
+          <h1 className="text-2xl font-bold mb-4">Add Custom Filter</h1>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={(e: any) => {
+              e.preventDefault();
+              p.onSubmit({ name: e.target["name"].value });
+            }}
+          >
+            <div className="flex flex-col gap-2">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                className={`input input-primary input-bordered`}
+              />
+            </div>
+            <label className="label">
+              The current column filters, advanced filters, and column sorting
+              currently applied to the table will be saved for immediate use.
+            </label>
+            <div className="flex justify-end gap-4 mt-4">
+              <button
+                className="btn btn-neutral text-neutral-content"
+                onClick={() => p.setShowModule(false)}
+                type="reset"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="btn btn-primary text-primary-content"
+                onClick={() => p.setShowModule(false)}
+              >
+                Set Custom Filter
+              </button>
+            </div>
+          </form>
+        </div>
+      </dialog>
+    </>
+  );
+};
--- openipam/frontend/src/modules/hosts/quickFilters.tsx
+++ openipam/frontend/src/modules/hosts/quickFilters.tsx
@@ -1,5 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
+import { Add } from "@mui/icons-material";
+import { AddCustomFilterModule } from "./addCustomFilterModule";
+
+type filterBtn = {
+  label: string;
+  filter: any[];
+};
 
 export const QuickFilters = (p: {
   setFilter: (filter: string[][]) => void;
@@ -7,8 +14,9 @@ export const QuickFilters = (p: {
   setCustomFilters: (filters: any) => void;
 }) => {
   const [active, setActive] = useState<string | undefined>();
+  const [showModule, setShowModule] = useState<boolean>(false);
   const auth = useAuth();
-  const [buttons, setButtons] = useState<any>([
+  const [buttons, setButtons] = useState<filterBtn[]>([
     {
       label: "Mine",
       filter: [["mine", auth?.username]],
@@ -21,52 +29,112 @@ export const QuickFilters = (p: {
       label: "Changed By Me",
       filter: [["changed_by", auth?.username]],
     },
-    {
-      label: "Custom",
-      filter: JSON.parse(localStorage.getItem("customHostQuickFilter") ?? "[]"),
-    },
   ]);
+  useEffect(() => {
+    if (!auth) return;
+    setButtons([
+      {
+        label: "Mine",
+        filter: [["mine", auth.username]],
+      },
+      {
+        label: "Group",
+        filter: [["show_groups", true]],
+      },
+      {
+        label: "Changed By Me",
+        filter: [["changed_by", auth.username]],
+      },
+    ]);
+  }, [auth]);
+  const [custom, setCustom] = useState<filterBtn[]>(
+    JSON.parse(localStorage.getItem("customHostQuickFilterButtons") ?? "[]")
+  );
   return (
     <div className="flex flex-col gap-2 mt-2 justify-center">
       <div className="flex flex-row gap-2 justify-between">
         <label className="label">Quick Filters:</label>
+        {custom.map((c) => c.label).includes(active ?? "") && (
+          <button
+            className="btn btn-ghost text-xs btn-sm mt-1 border-error"
+            onClick={() => {
+              localStorage.setItem(
+                "customHostQuickFilterButtons",
+                JSON.stringify(custom.filter((c) => c.label !== active))
+              );
+              setCustom(custom.filter((c) => c.label !== active));
+              setActive(undefined);
+            }}
+          >
+            Delete Active Custom Filter
+          </button>
+        )}
+      </div>
+      <div className="flex flex-row gap-2">
+        <div className="flex flex-row btn-group btn-group-horizontal">
+          {buttons.map((b: filterBtn) => (
+            <button
+              key={b.label}
+              onClick={() => {
+                p.setCustomFilters({});
+                setActive((prev) => (prev === b.label ? undefined : b.label));
+                p.setFilter(b.filter);
+              }}
+              className={`btn
+            ${active === b.label ? "btn-primary focus" : "btn-outline"}`}
+            >
+              {b.label}
+            </button>
+          ))}
+          {custom.map((b: filterBtn) => (
+            <button
+              key={b.label}
+              onClick={() => {
+                setActive((prev) => (prev === b.label ? undefined : b.label));
+                p.setFilter([]);
+                p.setCustomFilters(b.filter);
+              }}
+              className={`btn
+            ${active === b.label ? "btn-primary focus" : "btn-outline"}`}
+            >
+              {b.label}
+            </button>
+          ))}
+        </div>
         <button
-          className="btn btn-ghost text-xs btn-sm mt-1"
+          className="btn btn-ghost btn-sm btn-circle mt-2"
           onClick={() => {
-            localStorage.setItem(
-              "customHostQuickFilter",
-              JSON.stringify(p.currentFilters)
-            );
-            setButtons((prev: any) =>
-              prev.map((b: any) =>
-                b.label === "Custom" ? { ...b, filter: p.currentFilters } : b
-              )
-            );
-            p.setCustomFilters(p.currentFilters);
+            setShowModule(true);
           }}
         >
-          Set Custom
+          <Add />
         </button>
       </div>
-      <div className="flex flex-row btn-group btn-group-horizontal">
-        {buttons.map((b: any) => (
-          <button
-            key={b.label}
-            onClick={() => {
-              setActive((prev) => (prev === b.label ? undefined : b.label));
-              if (b.label === "Custom") {
-                p.setCustomFilters(b.filter);
-              } else {
-                p.setFilter(b.filter);
-              }
-            }}
-            className={`btn
-              ${active === b.label ? "btn-primary focus" : "btn-outline"}`}
-          >
-            {b.label}
-          </button>
-        ))}
-      </div>
+      <AddCustomFilterModule
+        showModule={showModule}
+        setShowModule={setShowModule}
+        onSubmit={({ name }) => {
+          console.log("got", name);
+          localStorage.setItem(
+            "customHostQuickFilterButtons",
+            JSON.stringify([
+              ...custom,
+              {
+                label: name,
+                filter: p.currentFilters,
+              },
+            ])
+          );
+          setCustom((prev: filterBtn[]) => [
+            ...prev,
+            {
+              label: name,
+              filter: p.currentFilters,
+            },
+          ]);
+          p.setCustomFilters(p.currentFilters);
+        }}
+      />
     </div>
   );
 };
