f9b2ae66e55a81fec66d40e2dbe81c1c594a3734
Lizzy Hunt <lizzy.hunt@usu.edu>
2023-11-03 14:25:40 -0600

    chunk batch deletion requests of expired hosts to bypass nginx max body size for form data

--- openipam/report/static/report/js/host/multiselect_host_form.js
+++ openipam/report/static/report/js/host/multiselect_host_form.js
@@ -1,27 +1,58 @@
+const chunk = (arr, chunkSize) => {
+  const chunks = [];
+  let chunk = 0;
+  for (let i = 0; i < arr.length; ++i, chunk = Math.floor(i / chunkSize)) {
+    const curr = arr[i];
+    if (chunk > chunks.length - 1) {
+      chunks.push([curr]);
+      continue;
+    }
+    chunks[chunk].push(curr);
+  }
+  return chunks;
+};
+
 const toggleAllCheckboxes = (check = true) =>
   $("input[type=checkbox]").each(function () {
     $(this).prop("checked", check);
   });
 
-const macCount = (form) => $(form)
-  .serializeArray()
-  .filter((input) => input.name === "mac_addr[]")
-  .length;
+const macCount = (form) =>
+  $(form)
+    .serializeArray()
+    .filter((input) => input.name === "mac_addr[]").length;
 
-const submitForm = (form) =>
-  new Promise((res, rej) => {
-    $.ajax({
-      url: $(form).attr("action"),
-      type: "POST",
-      data: $(form).serialize(),
-      success: (data) => res(data),
-      error: (data) => rej(data),
-    })
-  });
+const submitForm = (form) => {
+  // workaround for nginx 400 when mac_addr array is too long; split it into chunks and request
+  const chunkSize = 200;
+  const submitPromises = chunk($(form).serializeArray("mac_addr"), chunkSize)
+    .map((chunkFormData) =>
+      chunkFormData
+        .filter(({ name }) => name == "mac_addr[]")
+        .map(({ value }) => value)
+    )
+    .map((chunk) => {
+      const thisChunkData = new FormData($(form).get(0));
+      thisChunkData.delete("mac_addr[]");
+
+      chunk.forEach((macAddr) => thisChunkData.append("mac_addr[]", macAddr));
+
+      return new Promise((res, rej) => {
+        $.ajax({
+          url: $(form).attr("action"),
+          type: "POST",
+          data: new URLSearchParams(thisChunkData).toString(),
+          success: (data) => res(data),
+          error: (data) => rej(data),
+        });
+      });
+    });
+  return Promise.all(submitPromises);
+};
 
 const toggle = {
   "btn-primary": "btn-warning",
-  "btn-warning": "btn-primary"
+  "btn-warning": "btn-primary",
 };
 
 $("#toggle-checks").on("click", function () {
